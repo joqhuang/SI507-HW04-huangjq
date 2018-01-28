a19df4031f8d610c1c58f76c9cf7421e172738b4
Jackson Huang <questionableinsanity@gmail.com>
2018-01-28 14:39:46 -0500

    Added code to test that user input is a question

--- magic_eight.py
+++ magic_eight.py
@@ -1,2 +1,9 @@
-def get_user_question(user_q = input("What is your question? "))
+def get_user_question():
+    user_q = input("What is your question? ")
     return user_q
+
+user_question = ""
+while user_question != "quit":
+    user_question = get_user_question()
+    if user_question[-1] != "?":
+        print("I'm sorry, I can only answer questions. Or enter quit to end the program.")
