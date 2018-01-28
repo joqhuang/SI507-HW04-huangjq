dc0c7617c311a146b0e73e19ec395f6eec597751
Jackson Huang <questionableinsanity@gmail.com>
2018-01-28 16:58:06 -0500

    create magic_eight.py to get user question

--- magic_eight.py
+++ magic_eight.py
@@ -1,18 +1,3 @@
-import random
-
-PossibleAnswers = ["It is certain", "It is decidedly so", "Without a doubt", "Yes definitely", "You may rely on it", "As I see it, yes", "Most likely", "Outlook good", "Yes", "Signs point to yes", "Reply hazy try again", "Ask again later", "Better not tell you now", "Cannot predict now", "Concentrate and ask again", "Don\'t count on it", "My reply is no", "My sources say no", "Outlook not so good", "Very doubtful"]
-
 def get_user_question():
     user_q = input("What is your question? ")
     return user_q
-
-def PickRandomAnswer(Answers):
-    return random.choice(Answers)
-
-user_question = ""
-while user_question != "quit":
-    user_question = get_user_question()
-    if user_question[-1] != "?":
-        print("I'm sorry, I can only answer questions. Or enter quit to end the program.")
-    else:
-        print(PickRandomAnswer(PossibleAnswers))
